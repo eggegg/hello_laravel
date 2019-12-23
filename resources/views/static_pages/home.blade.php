86d572f80a6447e112a828a5340185eaf2a1957e
Egg Egg <eggegg01@126.com>
2019-12-23 17:09:34 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            这里是微博
+        </p>
+        <p>
+            一切从这里开始
+        </p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">现在注册 </a> </p>
+    </div>
 @stop
