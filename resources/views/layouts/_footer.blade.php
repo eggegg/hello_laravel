a668c37bd141b50d2a5231aba942be4f2a75729f
Egg Egg <eggegg01@126.com>
2019-12-23 18:56:37 +0800

    解决浏览器缓存问题

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <a href="https://laravel-china.org/courses" target=_blank>
+        每日练习
+    </a>
+    <div class="float-right"> <a href="/about" >关于 </a>
+    </div>
+</footer>
