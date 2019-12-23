58f0803f71bce09bc5f411aaa6e6882a6fc6751e
Egg Egg <eggegg01@126.com>
2019-12-23 19:00:01 +0800

    切割头部和尾部子视图

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-    <a href="https://laravel-china.org/courses" target=_blank>
+    <a href="#" target=_blank>
         每日练习
     </a>
     <div class="float-right"> <a href="/about" >关于 </a>
